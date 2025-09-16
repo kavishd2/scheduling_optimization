ecd61f8b5ebdfc5934da2670720a5afcd3188425
Kavish <kavish.dewani12@gmail.com>
2025-09-16 14:36:28 -0500

    Finished

---
+++ popup.js
@@ -0,0 +1,288 @@
+let orgs = {
+  "Texas Consulting": [
+    {
+      type: "Info Session",
+      events: [
+        { start: "2025-09-03T18:00-05:00", end: "2025-09-03T18:30-05:00", location: "FAC 21" },
+        { start: "2025-09-09T19:00-05:00", end: "2025-09-09T19:30-05:00", location: "JGB 2.324" }
+      ]
+    },
+    {
+      type: "Coffee Chat",
+      events: [
+        { start: "2025-09-05T16:00-05:00", end: "2025-09-05T17:00-05:00", location: "TBD" },
+        { start: "2025-09-05T16:30-05:00", end: "2025-09-05T17:30-05:00", location: "TBD" },
+        { start: "2025-09-05T17:00-05:00", end: "2025-09-05T18:00-05:00", location: "TBD" },
+        { start: "2025-09-10T17:00-05:00", end: "2025-09-10T18:00-05:00", location: "TBD" },
+        { start: "2025-09-10T17:30-05:00", end: "2025-09-10T18:30-05:00", location: "TBD" },
+        { start: "2025-09-10T18:00-05:00", end: "2025-09-10T19:00-05:00", location: "TBD" }
+      ]
+    },
+    {
+      type: "Resume Review",
+      events: [
+        { start: "2025-09-11T18:00-05:00", end: "2025-09-11T19:00-05:00", location: "UTC 4.110" }
+      ]
+    }
+  ],
+
+  "Texas Umbra": [
+    {
+      type: "Info Session",
+      events: [
+        { start: "2025-09-02T19:00-05:00", end: "2025-09-02T19:30-05:00", location: "SZB 1.510" },
+        { start: "2025-09-10T19:00-05:00", end: "2025-09-10T19:30-05:00", location: "SZB 1.510" }
+      ]
+    },
+    {
+      type: "Coffee Chat",
+      events: [
+        { start: "2025-09-05T15:00-05:00", end: "2025-09-05T16:00-05:00", location: "Union Eastwoods" },
+        { start: "2025-09-05T15:30-05:00", end: "2025-09-05T16:30-05:00", location: "Union Eastwoods" },
+        { start: "2025-09-05T16:00-05:00", end: "2025-09-05T17:00-05:00", location: "Union Eastwoods" },
+        { start: "2025-09-11T18:00-05:00", end: "2025-09-11T19:00-05:00", location: "Union Eastwoods" },
+        { start: "2025-09-11T18:30-05:00", end: "2025-09-11T19:30-05:00", location: "Union Eastwoods" },
+        { start: "2025-09-11T19:00-05:00", end: "2025-09-11T20:00-05:00", location: "Union Eastwoods" }
+      ]
+    },
+    {
+      type: "Office Hours",
+      events: [
+        { start: "2025-09-12T20:00-05:00", end: "2025-09-12T21:00-05:00", location: "Zoom" }
+      ]
+    }
+  ],
+
+  "Texas 180 Degrees Consulting": [
+    {
+      type: "Info Session",
+      events: [
+        { start: "2025-09-02T18:30-05:00", end: "2025-09-02T19:00-05:00", location: "GSB 3.138" },
+        { start: "2025-09-09T18:00-05:00", end: "2025-09-09T18:30-05:00", location: "MEZ 1.306" }
+      ]
+    },
+    {
+      type: "Coffee Chat",
+      events: [
+        { start: "2025-09-04T18:00-05:00", end: "2025-09-04T19:00-05:00", location: "CBA Events room" },
+        { start: "2025-09-11T18:00-05:00", end: "2025-09-11T19:00-05:00", location: "CBA Events room" }
+      ]
+    }
+  ],
+
+  "Texas Undergraduate Consulting Group": [
+    {
+      type: "Info Session",
+      events: [
+        { start: "2025-09-02T18:00-05:00", end: "2025-09-02T18:30-05:00", location: "GSB 2.126" },
+        { start: "2025-09-08T19:00-05:00", end: "2025-09-08T19:30-05:00", location: "GSB 2.126" }
+      ]
+    },
+    {
+      type: "Coffee Chat",
+      events: [
+        { start: "2025-09-03T17:00-05:00", end: "2025-09-03T18:00-05:00", location: "Gong Cha" },
+        { start: "2025-09-03T17:30-05:00", end: "2025-09-03T18:30-05:00", location: "Gong Cha" },
+        { start: "2025-09-03T18:00-05:00", end: "2025-09-03T19:00-05:00", location: "Gong Cha" },
+        { start: "2025-09-06T17:00-05:00", end: "2025-09-06T18:00-05:00", location: "Gong Cha" },
+        { start: "2025-09-06T17:30-05:00", end: "2025-09-06T18:30-05:00", location: "Gong Cha" },
+        { start: "2025-09-06T18:00-05:00", end: "2025-09-06T19:00-05:00", location: "Gong Cha" }
+      ]
+    },
+    {
+      type: "Resume/App Writing",
+      events: [
+        { start: "2025-09-10T17:00-05:00", end: "2025-09-10T18:00-05:00", location: "Gong Cha" }
+      ]
+    }
+  ],
+
+  "Technology Consulting Group": [
+    {
+      type: "Info Session",
+      events: [
+        { start: "2025-09-03T19:00-05:00", end: "2025-09-03T19:30-05:00", location: "UTC 3.122" },
+        { start: "2025-09-09T18:00-05:00", end: "2025-09-09T18:30-05:00", location: "UTC 3.134" }
+      ]
+    },
+    {
+      type: "Coffee Chat",
+      events: [
+        { start: "2025-09-04T18:30-05:00", end: "2025-09-04T19:30-05:00", location: "Gong Cha" },
+        { start: "2025-09-04T19:00-05:00", end: "2025-09-04T20:00-05:00", location: "Gong Cha" },
+        { start: "2025-09-04T19:30-05:00", end: "2025-09-04T20:30-05:00", location: "Gong Cha" },
+        { start: "2025-09-09T19:00-05:00", end: "2025-09-09T20:00-05:00", location: "Gong Cha" },
+        { start: "2025-09-09T19:30-05:00", end: "2025-09-09T20:30-05:00", location: "Gong Cha" },
+        { start: "2025-09-09T20:00-05:00", end: "2025-09-09T21:00-05:00", location: "Gong Cha" }
+      ]
+    },
+    {
+      type: "Office Hours",
+      events: [
+        { start: "2025-09-10T18:00-05:00", end: "2025-09-10T19:00-05:00", location: "Zoom" },
+      ]
+    }
+    ]
+};
+
+// Dom Elements
+const orgDropdown = document.getElementById("orgDropdown");
+const submitBtn = document.getElementById("submitBtn");
+const emailInput = document.getElementById("emailInput");
+
+function escapeId(s) {
+    return String(s).replace(/\s+/g, "-");
+}
+
+function buildChecklist() {
+    for (let org in orgs) {
+        // Category checkbox
+        const safeId = "${escapeId(org)}";
+        const orgDiv = document.createElement("div");
+
+        const orgCheckbox = document.createElement("input");
+        orgCheckbox.type = "checkbox";
+        orgCheckbox.id = safeId;
+        orgCheckbox.classList.add("org-checkbox");
+        orgCheckbox.value = org;
+
+        const orgLabel = document.createElement("label");
+        orgLabel.htmlFor = safeId;
+        orgLabel.textContent = org;
+
+        orgDiv.appendChild(orgCheckbox);
+        orgDiv.appendChild(orgLabel);
+        orgDropdown.appendChild(orgDiv);
+    }
+}
+buildChecklist();
+
+function generateBundles(org) {
+  let bundles = [[]]; 
+  for (let { type, events } of org) {
+    const newBundles = [];
+    for (let bundle of bundles) {
+      for (let event of events) {
+        newBundles.push([...bundle, { ...event, type }]);
+      }
+    }
+    bundles = newBundles;
+  }
+  return bundles;
+}
+
+function eventsOverlap(ev1, ev2) {
+  const start1 = new Date(ev1.start).getTime();
+  const end1 = new Date(ev1.end).getTime();
+  const start2 = new Date(ev2.start).getTime();
+  const end2 = new Date(ev2.end).getTime();
+  return start1 < end2 && start2 < end1;
+}
+
+function bundleConflicts(bundle1, bundle2) {
+  for (let ev1 of bundle1) {
+    for (let ev2 of bundle2) {
+      if (eventsOverlap(ev1, ev2)) return true;
+    }
+  }
+  return false;
+}
+
+function buildConflictGraph(selected) {
+  const nodes = [];
+  const edges = new Map();
+
+  selected.forEach(org => {
+    const bundles = generateBundles(orgs[org]);
+    bundles.forEach((bundle, bundleIdx) => {
+      nodes.push({ org, bundleIdx, bundle });
+    });
+  });
+
+  for (let i = 0; i < nodes.length; i++)
+    edges.set(i, new Set());
+  for (let i = 0; i < nodes.length; i++) {
+    for (let j = i + 1; j < nodes.length; j++) {
+      const nodeA = nodes[i];
+      const nodeB = nodes[j];
+      if (nodeA.org === nodeB.org || bundleConflicts(nodeA.bundle, nodeB.bundle)) {
+        edges.get(i).add(j);
+        edges.get(j).add(i);
+      }
+    }
+  }
+  return { nodes, edges };
+}
+
+function maxIndependentSet(nodes, edges) {
+  let bestSet = [];
+
+  function backtrack(currentSet = [], index = 0) {
+    if (index === nodes.length) {
+      if (currentSet.length > bestSet.length) bestSet = [...currentSet];
+      return;
+    }
+    if (currentSet.length + nodes.length - index < bestSet.length)
+        return;
+    backtrack(currentSet, index + 1);
+
+    const conflicts = edges.get(index);
+    if (!currentSet.some(i => conflicts.has(i))) {
+      backtrack([...currentSet, index], index + 1);
+    }
+  }
+
+  backtrack();
+  return bestSet.map(idx => nodes[idx]);
+}
+
+function generateICS(events) {
+  let ics = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Student Org Scheduler//EN\n";
+  events.forEach(ev => {
+    const dtStart = new Date(ev.start).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
+    const dtEnd = new Date(ev.end).toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
+    ics += "BEGIN:VEVENT\n";
+    ics += `SUMMARY:${ev.type} (${ev.org})\n`;
+    ics += `DTSTART:${dtStart}\n`;
+    ics += `DTEND:${dtEnd}\n`;
+    if (ev.location) ics += `LOCATION:${ev.location}\n`;
+    ics += "END:VEVENT\n";
+  });
+  ics += "END:VCALENDAR";
+  return ics;
+}
+
+function downloadICS(events) {
+  const icsContent = generateICS(events);
+  const blob = new Blob([icsContent], { type: "text/calendar" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "schedule.ics";
+  a.click();
+
+  URL.revokeObjectURL(url);
+}
+
+
+function optimizeSchedule() {
+    let selected = [];
+    const orgBoxes = document.querySelectorAll(".org-checkbox:checked");
+    orgBoxes.forEach(box => selected.push(box.value));
+
+    if (selected.length === 0) {
+        alert("Please select at least one organization.");
+        return;
+    }
+
+    const { nodes, edges } = buildConflictGraph(selected);
+    const optimizedMIS = maxIndependentSet(nodes, edges);
+    const events = [];
+    optimizedMIS.forEach(node => {
+    node.bundle.forEach(ev => events.push({ ...ev, org: node.org }));
+    });
+    downloadICS(events);
+    alert(`Optimization run for:\n${selected.join(", ")}\n\nCalendar downloaded. Please import for use`);
+}
+submitBtn.addEventListener("click", optimizeSchedule);
